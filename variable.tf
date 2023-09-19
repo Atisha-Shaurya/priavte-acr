b80a4d391786f9d11ac87407732971e958303d6a
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-09-19 14:59:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,17 +7,19 @@ variable "tags" {
 variable "location" {
   description = "Azure region where resources should be provisioned"
   type        = string
-  default     = "UK South"
+  default     = "eastus"
 }
 
 variable "azure_resource_group" {
   description = "Name of the Azure resource group where resources will be provisioned"
   type = string
+  default = "acr-demo-rg"
 }
 
 variable "acr_name" {
   description = "Name of the Azure Container Registry"
   type = string
+  default = "acr-demo"
 }
 variable "acr_sku" {
   description = "SKU of the Azure Container Registry"
